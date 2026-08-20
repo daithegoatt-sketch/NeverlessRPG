cf517774b7c3ebeef58cf8a3d581b03df80316bf
daithegoatt-sketch <daithegoatt@gmail.com>
2026-08-20 12:22:16 +0300

    Migrate player saves to V3 persistent account format

--- src/db/store.js
+++ src/db/store.js
@@ -19,34 +19,18 @@ function makePlayer(id, username = 'Traveler') {
   for (const wid of STARTER.weapons) ownedWeapons[wid] = { id: wid, copies: 1, level: 1, refinement: 1 };
   const ownedCharacters = {};
   for (const cid of STARTER.characters) {
-    ownedCharacters[cid] = {
-      id: cid,
-      level: 20,
-      constellation: 0,
-      weaponId: starterWeaponFor(cid),
-      artifactIds: [],
-      talents: defaultTalents(),
-    };
+    ownedCharacters[cid] = { id: cid, level: 20, constellation: 0, weaponId: starterWeaponFor(cid), artifactIds: [], talents: defaultTalents() };
   }
-
   return {
-    id,
-    username,
-    createdAt: Date.now(),
-    primogems: STARTER.primogems,
-    mora: STARTER.mora,
-    resin: STARTER.resin,
-    adventureXp: 0,
+    id, username, createdAt: Date.now(),
+    primogems: STARTER.primogems, mora: STARTER.mora, resin: STARTER.resin, adventureXp: 0,
     passive: { atk: 0, hp: 0, critRate: 0, critDmg: 0, spd: 0 },
     pity: { character: 0, weapon: 0, characterFour: 0, weaponFour: 0, characterGuaranteed: false },
-    characters: ownedCharacters,
-    weapons: ownedWeapons,
-    artifacts: {},
-    history: { wishes: [], battles: [] },
-    pvp: { wins: 0, losses: 0, mmr: 1000 },
-    showcase: STARTER.characters.slice(0, 4),
-    domainProgress: {},
-    ui: { profileMessageId: null, version: 2 },
+    characters: ownedCharacters, weapons: ownedWeapons, artifacts: {},
+    history: { wishes: [], battles: [] }, pvp: { wins: 0, losses: 0, mmr: 1000 },
+    showcase: STARTER.characters.slice(0, 4), domainProgress: {},
+    daily: { lastClaimAt: 0, streak: 0 },
+    ui: { profileMessageId: null, version: 3 },
   };
 }
 
@@ -59,8 +43,9 @@ function migratePlayer(player) {
   player.history.battles ||= [];
   player.pvp ||= { wins: 0, losses: 0, mmr: 1000 };
   player.domainProgress ||= {};
-  player.ui ||= { profileMessageId: null, version: 2 };
-  if (player.ui.version !== 2) { player.ui.version = 2; changed = true; }
+  if (!player.daily) { player.daily = { lastClaimAt: 0, streak: 0 }; changed = true; }
+  if (!player.ui) { player.ui = { profileMessageId: null, version: 3 }; changed = true; }
+  if (player.ui.version !== 3) { player.ui.version = 3; changed = true; }
   player.characters ||= {};
   player.weapons ||= {};
   player.artifacts ||= {};
@@ -74,13 +59,8 @@ function migratePlayer(player) {
   }
   const valid = new Set(Object.keys(player.characters));
   const showcase = Array.isArray(player.showcase) ? player.showcase.filter((id, i, a) => valid.has(id) && a.indexOf(id) === i).slice(0, 10) : [];
-  if (!showcase.length) {
-    player.showcase = Object.keys(player.characters).slice(0, 10);
-    changed = true;
-  } else if (JSON.stringify(showcase) !== JSON.stringify(player.showcase)) {
-    player.showcase = showcase;
-    changed = true;
-  }
+  if (!showcase.length) { player.showcase = Object.keys(player.characters).slice(0, 10); changed = true; }
+  else if (JSON.stringify(showcase) !== JSON.stringify(player.showcase)) { player.showcase = showcase; changed = true; }
   return changed;
 }
 
@@ -92,29 +72,20 @@ export class JsonStore {
     this.data = JSON.parse(fs.readFileSync(this.file, 'utf8'));
     this.data.players ||= {};
   }
-
   save() {
     const tmp = `${this.file}.tmp`;
     fs.writeFileSync(tmp, JSON.stringify(this.data, null, 2));
     fs.renameSync(tmp, this.file);
   }
-
   getPlayer(id, username) {
     let changed = false;
-    if (!this.data.players[id]) {
-      this.data.players[id] = makePlayer(id, username);
-      changed = true;
-    }
+    if (!this.data.players[id]) { this.data.players[id] = makePlayer(id, username); changed = true; }
     const player = this.data.players[id];
     if (migratePlayer(player)) changed = true;
-    if (username && player.username !== username) {
-      player.username = username;
-      changed = true;
-    }
+    if (username && player.username !== username) { player.username = username; changed = true; }
     if (changed) this.save();
     return player;
   }
-
   mutatePlayer(id, mutator, username) {
     const player = this.getPlayer(id, username);
     const result = mutator(player);
